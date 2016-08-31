74fbbfbf3a88f78ba6abae3d5f2a95e7acbe27f9
sjono <jono.sanders@gmail.com>
2016-08-31 14:20:37 -0400

    Blossom_server - edited to run even with an empty test.json

--- blossom_server.py
+++ blossom_server.py
@@ -9,16 +9,22 @@ app = Flask(__name__)
 
 import os
 import json
-tweet_ct = 0
+
 
 @app.route("/")
 def counter():
 	tweets = []
+	tweet_ct = 0
 	#with tweets exported to json like this >>
 	#json.dump(tweets, open("test.json", "w"))
 	with open("test.json") as data_file:
-		data = json.load(data_file)
-	tweet_ct = len(data)
+		try: 
+			data = json.load(data_file)
+		except:
+			data = 0
+			print "Empty JSON file"
+	if (data != 0):
+		tweet_ct = len(data)
 	return "Tweet count is: " + str(tweet_ct)
 if __name__ == "__main__":
 	app.run()
