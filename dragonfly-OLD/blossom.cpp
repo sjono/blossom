d952fb2e64f0b753d13cb41650459a66a1ddc8b6
Jono Sanders <jono.sanders@gmail.com>
2017-09-04 08:49:38 -0400

    Updating folders
    Put server files in one folder.
    Added the old Multitech Dragonfly code
    First commit for new Electron firmware

---
+++ dragonfly-OLD/blossom.cpp
@@ -0,0 +1,369 @@
+/*
+ Name: Dragonfly HTTP Full (main.cpp)
+ # Author: Jono + Phil
+ # Date: Sep 13 2016
+ # Description: Code to connect to HTTP for updated tweet counts
+ # derived from Dragonfly Cellular HTTP Example
+ # 830p - JS added code to send SMS
+ # 09/15 11:45am  JS changing opening speed to reflect other timing example, fix open duration
+ # 09/18 Run on SMS
+*/
+
+#include "mbed.h"
+#include "mtsas.h"
+#include "led.h"
+int StringParse(string input);
+int IntParse(char* input);
+// This line controls the regulator's battery charger.
+// BC_NCE = 0 enables the battery charger
+// BC_NCE = 1 disables the battery charger
+DigitalOut bc_nce(PB_2);
+
+bool init_mtsas();
+char* httpResToStr(HTTPResult res);
+
+// The MTSSerialFlowControl object represents the physical serial link between the processor and the cellular radio.
+mts::MTSSerialFlowControl* io;
+// The Cellular object represents the cellular radio.
+mts::Cellular* radio;
+
+// An APN is required for GSM radios. This works for AT&T CARD!
+static const char apn[] = "wap.cingular";
+static std::string phone_number = "15039620846";
+
+bool radio_ok = false;
+Timer timer1; // For counting the HTTP requests
+string twilioNum = "\"+12672140103\"";
+
+int main() {
+    //~~~~INITIALIZE VARIABLES~~~~~~
+
+    //LED VARIABLES
+    int r, g, b;
+    data05 = 0;                            //Initialize direction CLOSE
+    data04 = 1;                            //Initialize motor on
+    wait(16);         // Close motor for 8 sec
+    data04 = 0;         // turn motor off
+    int open_ct = 0;
+
+    r = 0;
+    g = 160;
+    b = 0;
+    int change = 10;
+    ledChaseTail(160,0,0,50,6); // Run ledChase once at the start
+    ledChaseTail(160,0,0,50,6); // Run ledChase once at the start
+    ledChaseTail(160,0,0,50,6); // Run ledChase once at the start
+    //END LED VARIABLES
+
+    // Disable the battery charger unless a battery is attached.
+    bc_nce = 1;
+    int http_ct = 0; // Counter for the number of times the board connects to HTTP
+    string sms = "0"; // store any incoming SMS message
+    string incomingNum = "0"; //store incoming phone number
+    int feed_count = 0; //  integer version of web_count
+    int temp_count = 0; // Store previous count
+    int water = 0; // Count new waters each time
+    // Change the baud rate of the debug port from the default 9600 - switch to 115200 later to reduce noise
+    Serial debug(USBTX, USBRX);
+    debug.baud(9600);
+
+    //Sets the log level to TRACE, for higher outputs
+    //Possible levels: NONE, FATAL, ERROR, WARNING, INFO, DEBUG, TRACE
+    mts::MTSLog::setLogLevel(mts::MTSLog::TRACE_LEVEL);
+    timer1.start();
+    logInfo("initializing cellular radio");
+    radio_ok = init_mtsas();
+    if (! radio_ok) {
+        while (true) {
+            logError("failed to initialize cellular radio");
+            wait(1);
+        }
+    }
+
+    logInfo("setting APN");
+    if (radio->setApn(apn) != MTS_SUCCESS)
+        logError("failed to set APN to \"%s\"", apn);
+
+    logInfo("bringing up the link");
+    if (! radio->connect()) {
+        logError("failed to bring up the link");
+    }
+    else {
+        mts::Cellular::Sms msg; //Cell SMS setup
+        msg.phoneNumber = phone_number;
+
+    // HTTPClient object used for HTTP requests.
+        HTTPClient http;
+        //~~~~~~~~~~~~  END INITIALIZE ~~~~~~~~~
+        while(1) //MAIN LOOP FOR READING HTTP & SMS
+            {
+            ledBreathLinear(0,70,80,100,6); // run this (100 means a little longer)
+            colorWipe(0,70,80,50); // turqoise means entered main lo op
+            // HTTP GET
+
+//            //~~~~~~~ READ HTTP ~~~~~~~~~~~
+//            char http_rx_buf[1024];
+//            HTTPResult res;
+//
+//            // IHTTPDataIn object - will contain data received from server.
+//            HTTPText http_rx(http_rx_buf, sizeof(http_rx_buf));
+//
+//            // Make a HTTP GET request to http://guarded-eyrie-61449.herokuapp.com
+//            res = http.get("http://guarded-eyrie-61449.herokuapp.com/", &http_rx);
+//            if (res != HTTP_OK)
+//                logError("HTTP GET failed [%d][%s]", res, httpResToStr(res));
+//            else {
+//                logInfo("HTTP GET succeeded [%d]\r\n%s", http.getHTTPResponseCode(), http_rx_buf);
+//                http_ct++; // Incremenets http read count when successful
+//                if (http_ct >= 10){ // Sends one SMS of http text after 10 successful reads
+//                    msg.message = http_rx_buf;
+//                    if (radio->sendSMS(msg) != MTS_SUCCESS) //Send HTTP results thru SMS
+//                        logError("sending SMS failed");
+//                    http_ct = 0;
+//                    }
+//                //read_count = strtok(http_rx_buf, " "); // Stores feed_count number
+//                temp_count = feed_count; // Store previous count
+//                feed_count = IntParse(http_rx_buf);
+//                if (feed_count > temp_count) // Increment only if the feed_count is larger
+//                    water += (feed_count - temp_count)/4; //dampened b/c of counts
+//                logInfo("**Total Count from HTTP is: %d", feed_count);
+//                }
+//            //~~~END READING HTTP~~~~~~~~~
+
+            //~~~~~~~ CHECK FOR SMS ~~~~~
+            // Display any received SMS messages.
+
+            std::vector<mts::Cellular::Sms> msgs = radio->getReceivedSms();
+            for (std::vector<mts::Cellular::Sms>::iterator it = msgs.begin(); it != msgs.end(); it++) {
+                sms = it->message.c_str();
+                incomingNum = it->phoneNumber.c_str();
+                logInfo("[%s][%s]\r\n%s\r\n", incomingNum, it->timestamp.c_str(), sms);
+                logInfo("Are there quotes on number: %s and twilio: %s", incomingNum, twilioNum);
+                if (twilioNum.compare(incomingNum)==0){
+                    logInfo("Text from twilio!");
+                    temp_count = feed_count; // Store previous count
+                    feed_count = StringParse(sms);
+                    if (feed_count > temp_count)
+                        water+= (feed_count - temp_count); // Increment water 2x for every SMS received
+                        logInfo("New watering! old count: %d, new count: %d", temp_count, feed_count);
+                    }
+                else {
+                    logInfo("text from other, will open");
+                    water+=2;
+                }
+                }
+            radio->deleteOnlyReceivedReadSms();
+            // Finish HTTP Get
+            logInfo("Running ledChaseTail");
+            ledChaseTail(r,g,b,50,6);
+            ledBreathLinear(r,g,b,100,6); // run this (100 means a little longer)
+            //~~~~~~~INCREMENT MOTORS AND OUTPUT LEDS~~~~~~~~~~
+            timer1.reset(); //restart timer
+            //~~~~~~~~OPEN THE PETAL!!!~~~~~~~~~~~~~~~~~~~
+            while(water >= 1)
+                {
+                change = 10;
+                if (water+open_ct > 20)    //To make sure it doesnt over-open
+                    water = 20 - open_ct;
+                if (g > 8)
+                    g -= change;
+                else g = 0;
+                if (r < 172)
+                    r += change;
+                else r = 180;
+                ledChaseTail(r,g,b,50,6);
+                printf("\n Timer is %d, water is %d", timer1.read_ms(), water); //debug
+                printf("\n Opening: Counter is at %d, Water count is at %d R is %d, G is %d", open_ct, water, r, g);
+                data05 = 1;         // Set motor DIR to open
+                data04 = 1;         //Set motor EN to on
+                wait(0.8);
+                data04 = 0; // Stop the motors
+                open_ct ++;
+                water = water-1;  // Use up one water count
+                }
+            timer1.reset();
+            //~~~~~~~~~~~ CLOSE THE PETAL!! ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+            while (open_ct >= 20)   // If fully open, close it before connecting!
+                {
+                if (open_ct == 20)
+                    ledDetonate(r,g,b, 30, 200, 10, 10); // Would this be good?
+                    //ledDetonate(r,g,b, wait, interval, mult, flashes)
+                printf("\n Closing counter is %d.. R is %d, G is %d", open_ct, r, g);
+                data05 = 0;         // Set motor DIR to close
+                data04 = 1;         // motor on
+                // Close motor in 3 sec increments
+                if (timer1.read_ms() >= 3000){
+                    data04 = 0;         // Turn motor off for a second
+                    open_ct+=1;
+                    timer1.reset();          //Resets timer count to 0
+                    printf("\n Counter is at %d", open_ct);
+                }
+                // Done closing after 15 cycles of closing
+                if(open_ct > 30){
+                    data04 = 0; // Turn off motoro
+                    open_ct = 0;        //Reset open count
+                    timer1.reset();
+                    g = 180;
+                    r = 0;
+                    }
+                }
+
+            //~~~~~~~~END INCREMENT~~~~~~~~~~~~~~~~~~~~~
+
+            ledChaseTail(r,g,b,50,6);
+            ledBreathLinear(r,g,b,100,6); // run this (100 means a little longer)
+            logInfo("Looping back");
+
+            } // End main While loop
+
+        } // End loop if cell connection worked
+
+    logInfo("finished - bringing down link");
+    radio->disconnect();
+
+    return 0;
+}
+
+bool init_mtsas() {
+    io = new mts::MTSSerialFlowControl(RADIO_TX, RADIO_RX, RADIO_RTS, RADIO_CTS);
+    if (! io)
+        return false;
+
+    // radio default baud rate is 115200
+    io->baud(115200);
+    radio = mts::CellularFactory::create(io);
+    if (! radio)
+        return false;
+
+    // Transport must be set properly before any TCPSocketConnection or UDPSocket objects are created
+    Transport::setTransport(radio);
+
+    return true;
+}
+
+char* httpResToStr(HTTPResult res) {
+    switch(res) {
+        case HTTP_PROCESSING:
+            return "HTTP_PROCESSING";
+        case HTTP_PARSE:
+            return "HTTP_PARSE";
+        case HTTP_DNS:
+            return "HTTP_DNS";
+        case HTTP_PRTCL:
+            return "HTTP_PRTCL";
+        case HTTP_NOTFOUND:
+            return "HTTP_NOTFOUND";
+        case HTTP_REFUSED:
+            return "HTTP_REFUSED";
+        case HTTP_ERROR:
+            return "HTTP_ERROR";
+        case HTTP_TIMEOUT:
+            return "HTTP_TIMEOUT";
+        case HTTP_CONN:
+            return "HTTP_CONN";
+        case HTTP_CLOSED:
+            return "HTTP_CLOSED";
+        case HTTP_REDIRECT:
+            return "HTTP_REDIRECT";
+        case HTTP_OK:
+            return "HTTP_OK";
+        default:
+            return "HTTP Result unknown";
+    }
+}
+
+int IntParse(char* input)
+    {
+    // An optimized int parse method.
+    char* pString = input;
+    int result = 0;
+    for (int i = 0; i < 6; i++)
+    {
+        if (pString[i] != ' ')
+            result = 10 * result + (input[i] - 48);
+        else break;
+    }
+    return result;
+    }
+
+int StringParse(string input)
+    {
+    // An optimized int parse method.
+    //char pString = input;
+    int result = 0;
+    for (int i = 0; i < input.length(); i++)
+    {
+        if (input[i] != ' ')
+            result = 10 * result + (input[i] - 48);
+        else break;
+    }
+    return result;
+    }
+
+
+    void ledChaseTail (unsigned char r, unsigned char g, unsigned char b, unsigned int wait, int tail){
+      int pixelNum = 0;
+      int tail_curr = 0;
+      int tail_comp = tail + 1;
+      __disable_irq();    // Disable Interrupts
+      for (int c = 0; c < (PIXELS+tail); c++){
+        if (pixelNum < PIXELS+1){
+            for (int i = 0; i < (pixelNum-tail_curr); i++){
+                sendPixel(0,0,0);
+            }
+            for (int i = 0; i < tail_curr; i++){
+                sendPixel((r*(i+1)/(tail_comp)), (g*(i+1)/(tail_comp)), (b*(i+1)/(tail_comp)));
+            }
+            sendPixel(r,g,b);
+            for (int i = pixelNum; i < PIXELS; i++){
+                sendPixel(0,0,0);
+            }
+            show();
+            pixelNum++;
+            if (tail_curr < tail){
+                tail_curr++;
+            }
+        }
+        else {
+            for (int i = 0; i < (pixelNum-tail_curr); i++){
+                sendPixel(0,0,0);
+            }
+            for (int i = 0; i < tail_curr; i++){
+                sendPixel((r*(i+1)/(tail_comp)), (g*(i+1)/(tail_comp)), (b*(i+1)/(tail_comp)));
+            }
+            show();
+            //pixelNum++;
+            tail_curr--;
+        }
+        wait_ms(wait);
+      }
+      __enable_irq();     // Enable Interrupts
+    }
+
+    void ledBreathLinear(unsigned char r, unsigned char g, unsigned char b, unsigned int wait, int mult){
+        char r_int = r/mult;
+        char g_int = g/mult;
+        char b_int = b/mult;
+        __disable_irq();    // Disable Interrupts
+        for(int i = 0; i<mult; i++){
+            char r_curr = i * r_int;
+            char g_curr = i * g_int;
+            char b_curr = i * b_int;
+            for( int p=0; p<PIXELS; p++ ) {
+                sendPixel( r_curr , g_curr , b_curr );
+            }
+            show();
+            wait_ms(wait);
+        }
+        for(int i = mult; i>0; i--){
+            char r_curr = i * r_int;
+            char g_curr = i * g_int;
+            char b_curr = i * b_int;
+            for( int p=0; p<PIXELS; p++ ) {
+                sendPixel( r_curr , g_curr , b_curr );
+            }
+            show();
+            wait_ms(wait);
+        }
+        __enable_irq();     // Enable Interrupts
+    }
---
+++ electron/blossom-3g.ino
@@ -0,0 +1,272 @@
+
+#include <SparkIntervalTimer.h>
+#include <neopixel.h>
+
+/*
+ Name: Blossom Electron (blossom-3g.ino)
+ # Author: Jono + Phil
+ # Date: Aug 4 2017
+ # Description: Control the Blossom sculpture for LED outputs and motor drivers
+ # Gets tweet & instagram counts from particle subscription and responds accordingly
+ # 08/08 - Added code for ledChase tail >> this connects and reads gotCount, runs thru line 82 - add printLn after whiles?
+ # Next - test with a motor output, add colorWipe, add Detonate code
+*/
+
+
+int checkCount(String command);
+void gotCount(const char *name, const char *data);
+void ledChaseTail (unsigned char r, unsigned char g, unsigned char b, unsigned int wait, int tail);
+void blinkCount(void);
+
+#define PIXEL_COUNT 15
+#define PIXEL_PIN A5
+#define MOTOR_DIR A4
+#define MOTOR_EN A3
+#define MOTOR_INC 30 // motor increment is 3 sec (units are 1/10 seconds)
+#define PIXEL_TYPE WS2812B
+Adafruit_NeoPixel strip(PIXEL_COUNT, PIXEL_PIN, PIXEL_TYPE);
+IntervalTimer myTimer;		// 3 for the Core
+
+int counter = 0;
+
+void setup() {
+    pinMode(MOTOR_EN, OUTPUT);
+    pinMode(MOTOR_DIR, OUTPUT);
+
+  // AUTO allocate blinkLED to run every 100ms using hmSec timescale (1000 * 0.1ms period)
+    myTimer.begin(blinkCount, 00, hmSec);
+
+    strip.begin();
+    strip.show();
+    Particle.function("count", checkCount); // Begin serial communication
+    Serial.begin(9600);
+    Serial.println("Initialization >> setting up stuff!");
+    // Listen for the webhook response, and call gotCount()
+    Particle.subscribe("hook-response/getFeedBlossomCount", gotCount, MY_DEVICES);
+
+    Particle.publish("getFeedBlossomCount");  // check the count ourselves once
+
+    // Close petals during initialization to be sure they are all at zero
+    digitalWrite(MOTOR_DIR, LOW);  // Set motor DIR to close
+    digitalWrite(MOTOR_EN, HIGH); //Set motor EN to on
+    for(int i=0;i<16;i++) {
+        Serial.println("closing the petals, " + String(16-i) + " sec remaining");
+        delay(1000);
+        //set motor pin to 1
+    }
+    digitalWrite(MOTOR_EN, LOW); //Set motor EN to off
+    Serial.println("Finishing main loop");
+}
+
+void loop() {
+    int open_ct = 0; // for counting of openings
+    int water = 0;
+    int change = 0;
+    int r, g, b;
+    Serial.println("Starting loop - will led breath and then chase");
+
+    //OPTIONAL publish events to trigger webhook (if we want to check server w a specific freq)
+    //Particle.publish("getFeedBlossomCount");
+
+    // and wait at least 60 seconds before doing it again
+    // for(int i=0;i<4;i++) {
+    //     Serial.println("waiting " + String(20-i*5) + " seconds to repeat loop");
+    //     delay(5000);
+    // }
+
+    strip.setPixelColor(0, 50,50,50);
+    strip.show();
+    delay(1000);
+    //rainbow(20);
+
+    ledChaseTail(r,g,b,50,6);
+
+    // code from Blossom2016
+
+    // ~~~~~~~~~~~~~~~~~~ run initial "connecting pattern"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    // Run intro LED pattern to show initializations
+    //ledBreathLinear(0,70,80,100,6); // run this (100 means a little longer)
+    //colorWipe(0,70,80,50); // turqoise means entered main lo op
+
+    // Run chase tail to indicate "searching for tweets/instas"
+    //ledChaseTail(r,g,b,50,6);
+    //ledBreathLinear(r,g,b,100,6); // run this (100 means a little longer)
+    // ~~~~~~~~~~~~~~~~~~ end connecting pattern"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    // ~~~~~~~~~~~~~~~~~~ open based on watering count (make its own function)~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    while(water >= 1)
+        {
+        change = 10;
+        if (water+open_ct > 20)    //To make sure it doesnt over-open
+            water = 20 - open_ct;
+        if (g > 8)
+            g -= change;
+        else g = 0;
+        if (r < 172)
+            r += change;
+        else r = 180;
+        ledChaseTail(r,g,b,50,6);
+        Serial.println("Timer is " + String(counter*100) + ", water is " + String(water)); //debug
+        Serial.println("Opening: Counter is at" + String(open_ct) + ", Water count is at " +String(water) + " R is " +r+ ", G is " + String(g));
+        digitalWrite(MOTOR_DIR, HIGH);  // Set motor DIR to open
+        digitalWrite(MOTOR_EN, HIGH); //Set motor EN to on
+        delay(80); //
+        digitalWrite(MOTOR_EN, LOW);
+        open_ct ++;
+        water = water-1;  // Use up one water count
+        }
+    // ~~~~~~~~~~~~~~~~~~~~ End watering sequence ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    // ~~~~~~~~~~~~~~ Closing sequence - call if fully open ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    while (open_ct >= 20)
+        {
+        if (open_ct == 20)
+            //ledDetonate(r,g,b, 30, 200, 10, 10); // ~~~~~~~~~~~~~~~~~ ADD THIS FUNCTION !!!!!! ~~~~~~~~~~~~~~~~~~~~ !!!!!!!!!!!!
+            //ledDetonate(r,g,b, wait, interval, mult, flashes)
+        Serial.println("Closing counter is " + String(open_ct));
+        digitalWrite(MOTOR_DIR, LOW);         // Set motor DIR to close
+        digitalWrite(MOTOR_EN, HIGH);       // motor on
+        // Close motor in 3 sec increments
+        if (counter >= MOTOR_INC){ // after motor "open increment" (1/10 seconds)
+            digitalWrite(MOTOR_EN, LOW);       // motor off for a sec
+            open_ct+=1;
+            counter = 0;          //Resets timer count to 0
+            Serial.println("Counter is at "+ String(open_ct));
+        }
+        // Done closing after 15 cycles of closing
+        if(open_ct > 30){
+            digitalWrite(MOTOR_EN, LOW);       // motor off for a sec
+            open_ct = 0;        //Reset open count
+            counter = 0;
+            g = 180;
+            r = 0;
+            }
+        }
+      // ~~~~~~~~~~~~~~~~~~~~ End closing sequence ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+}
+
+// This function will get called when weather data comes in
+void gotCount(const char *name, const char *data) {
+    String str = String(data);
+
+    if (str != NULL) {
+        Serial.println("Data received was: ");
+        Serial.println(str);
+    }
+    else {
+        Serial.println("Data read but nothing");
+    }
+
+    //TO DO! add code to parse the incoming string and store counts, ref below
+
+    // for (std::vector<mts::Cellular::Sms>::iterator it = msgs.begin(); it != msgs.end(); it++) {
+    //     sms = it->message.c_str();
+    //     incomingNum = it->phoneNumber.c_str();
+    //     logInfo("[%s][%s]\r\n%s\r\n", incomingNum, it->timestamp.c_str(), sms);
+    //     logInfo("Are there quotes on number: %s and twilio: %s", incomingNum, twilioNum);
+    //     if (twilioNum.compare(incomingNum)==0){
+    //         logInfo("Text from twilio!");
+    //         temp_count = feed_count; // Store previous count
+    //         feed_count = StringParse(sms);
+    //         if (feed_count > temp_count)
+    //             water+= (feed_count - temp_count); // Increment water 2x for every SMS received
+    //             logInfo("New watering! old count: %d, new count: %d", temp_count, feed_count);
+    //         }
+    //     else {
+    //         logInfo("text from other, will open");
+    //         water+=2;
+    //     }
+}
+
+// this function automagically gets called upon a matching POST request
+int checkCount(String command)
+{
+  // look for the matching argument "coffee" <-- max of 64 characters long
+  if(command == "go")
+  {
+    Serial.println("CheckCount called - requesting count...");
+    Particle.publish("getFeedBlossomCount"); //!COMMENTED OUT, resume this if we want automatic GET
+    return 1;
+  }
+  else {
+      Serial.println("Function called, wrong argument - try 'go' ");
+      return -1;
+  }
+}
+
+void rainbow(uint8_t wait) {
+  uint16_t i, j;
+  for(j=0; j<256; j++) {
+    for(i=0; i<strip.numPixels(); i++) {
+      strip.setPixelColor(i, Wheel((i+j) & 255));
+    }
+    strip.show();
+    delay(wait);
+  }
+}
+
+// Input a value 0 to 255 to get a color value.
+// The colours are a transition r - g - b - back to r.
+uint32_t Wheel(byte WheelPos) {
+  if(WheelPos < 85) {
+   return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+  } else if(WheelPos < 170) {
+   WheelPos -= 85;
+   return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+  }
+}
+
+//strip.setPixelColor(pixel, r, g, b);
+void ledChaseTail (unsigned char r, unsigned char g, unsigned char b, unsigned int wait, int tail){
+     int pixelNum = 0;
+     int tail_curr = 0;
+     int tail_comp = tail + 1;
+     __disable_irq();    // Disable Interrupts
+     for (int c = 0; c < (PIXEL_COUNT+tail); c++){
+       if (pixelNum < PIXEL_COUNT+1){
+           for (int i = 0; i < (pixelNum-tail_curr); i++){
+                strip.setPixelColor(pixelNum, r, g, b);
+               //sendPixel(0,0,0);
+           }
+           for (int i = 0; i < tail_curr; i++){
+               strip.setPixelColor(pixelNum, (r*(i+1)/(tail_comp)), (g*(i+1)/(tail_comp)), (b*(i+1)/(tail_comp)));
+               //sendPixel((r*(i+1)/(tail_comp)), (g*(i+1)/(tail_comp)), (b*(i+1)/(tail_comp)));
+           }
+           strip.setPixelColor(pixelNum, r, g, b);
+           //sendPixel(r,g,b);
+           for (int i = pixelNum; i < PIXEL_COUNT; i++){
+               strip.setPixelColor(pixelNum, 0, 0, 0);
+               //sendPixel(0,0,0);
+           }
+           strip.show();
+           pixelNum++;
+           if (tail_curr < tail){
+               tail_curr++;
+           }
+       }
+       else {
+           for (int i = 0; i < (pixelNum-tail_curr); i++){
+               strip.setPixelColor(pixelNum, 0, 0, 0);
+              //  sendPixel(0,0,0);
+           }
+           for (int i = 0; i < tail_curr; i++){
+               strip.setPixelColor(pixelNum, (r*(i+1)/(tail_comp)), (g*(i+1)/(tail_comp)), (b*(i+1)/(tail_comp)));
+              //  sendPixel((r*(i+1)/(tail_comp)), (g*(i+1)/(tail_comp)), (b*(i+1)/(tail_comp)));
+           }
+           strip.show();
+           //pixelNum++;
+           tail_curr--;
+       }
+       delay(wait); // milliseconds
+     }
+   }
+
+void blinkCount() {     // called based on timer, currently every 0.1ms
+    counter++;		// increase
+    if (counter > 100) // 10 sec
+    {
+        counter = 0;
+    }
+}
